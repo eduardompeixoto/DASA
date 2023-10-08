520df501780c6680c292afa7004bb3795848db79
eduardompeixoto <46012106+eduardompeixoto@users.noreply.github.com>
2023-10-08 19:57:26 -0300

    Update esus.R

--- R/esus.R
+++ R/esus.R
@@ -6,10 +6,15 @@ require(sn)
 require(readr)  
 require(googlesheets4)
 googlesheets4::gs4_deauth()
+list(
+read_sheet("https://docs.google.com/spreadsheets/d/1Dk2CD75dgvPt11EAChbBcYXNe2xz8GjwG1YvdiFYWxA/edit#gid=1208927962",,
+                    sheet = "antigeno"),
 
-read_sheet("https://docs.google.com/spreadsheets/d/1Dk2CD75dgvPt11EAChbBcYXNe2xz8GjwG1YvdiFYWxA/edit#gid=1208927962")
-
-  
+ read_sheet("https://docs.google.com/spreadsheets/d/1Dk2CD75dgvPt11EAChbBcYXNe2xz8GjwG1YvdiFYWxA/edit#gid=1208927962",,
+                    sheet = "pcr_detectavel"),
+ 
+read_sheet("https://docs.google.com/spreadsheets/d/1Dk2CD75dgvPt11EAChbBcYXNe2xz8GjwG1YvdiFYWxA/edit#gid=1208927962",,
+                    sheet = "pcr_n_detectavel")
 
 }
   
